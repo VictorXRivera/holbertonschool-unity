86be9257534febe244f61701933f181b5be7fcfb
VictorXRivera <victorxrivera.professional@gmail.com>
2021-01-13 19:04:28 -0500

    Adding assets and project settings folders

---
+++ 0x04-unity_publishing/Assets/Scripts/CameraController.cs
@@ -0,0 +1,13 @@
+﻿using UnityEngine;
+
+public class CameraController : MonoBehaviour
+{
+    public GameObject player;
+    public Transform track_player;
+    public Vector3 offset;
+
+    void Update()
+    {
+        transform.position = track_player.position + offset;
+    }
+}
