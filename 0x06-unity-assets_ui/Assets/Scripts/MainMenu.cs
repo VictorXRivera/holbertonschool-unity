308e8efda6f4bca898b38db7299df31769367ef8
VictorXRivera <victorxrivera.professional@gmail.com>
2021-02-21 12:23:59 -0500

    Pushing scripts for main menu and options menu

---
+++ 0x06-unity-assets_ui/Assets/Scripts/MainMenu.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class MainMenu : MonoBehaviour
+{
+    public Button Level01;
+    public Button Level02;
+    public Button Level03;
+    public Button OptionsButton;
+    public Button Exit;
+
+    public void LevelSelect(int level)
+    {
+        SceneManager.LoadScene(level);
+    }
+
+    public void Options()
+    {
+        PlayerPrefs.SetString("lastLoadedScene", SceneManager.GetActiveScene().name);
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 4);
+    }
+    
+    public void ExitGame()
+    {
+        Debug.Log("Exited");
+        Application.Quit();
+    }
+}
---
+++ 0x06-unity-assets_ui/Assets/Scripts/OptionsMenu.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class OptionsMenu : MonoBehaviour
+{
+    public Button BackButton;
+    public void Back()
+    {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex - 4);
+    }
+}
