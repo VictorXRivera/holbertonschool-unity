2f33481c734f219cfde37d911bf626119633acca
VictorXRivera <victorxrivera.professional@gmail.com>
2022-05-05 09:43:19 -0400

    Unity scripting project

---
+++ 0x02-unity-scripting/Assets/Scripts/Rotator.cs
@@ -0,0 +1,10 @@
+﻿using UnityEngine;
+
+public class Rotator : MonoBehaviour
+{
+    public float rotate;
+    void Update()
+    {
+        transform.Rotate(rotate * Time.deltaTime, 0, 0);
+    }
+}
