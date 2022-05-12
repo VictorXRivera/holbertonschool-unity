88f729c4c969952a3300fda56779e13190d399be
VictorXRivera <victorxrivera.professional@gmail.com>
2022-05-12 13:08:28 -0400

    Uploading project

---
+++ 0x04-unity_publishing/Assets/Scripts/PlayerController.cs
@@ -0,0 +1,101 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+using System.Collections;
+public class PlayerController : MonoBehaviour
+{
+    public Rigidbody rb;
+    public float speed = 500f;
+    private int score = 0;
+    private int health = 5;
+    public Text scoreText;
+    public Text healthText;
+    public Text winLoseText;
+    public Image winLoseBG;
+
+    void SetScoreText()
+    {
+        scoreText.text = "Score: " + score.ToString();
+    }
+
+    void SetHealthText()
+    {
+        healthText.text = "Health: " + health.ToString();
+    }
+    void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.tag == "Pickup")
+        {
+            Destroy(other.gameObject);
+            score += 1;
+            SetScoreText();
+        }
+
+        if (other.gameObject.tag == "Trap")
+        {
+            health -=1;
+            SetHealthText();
+        }
+
+        if (other.gameObject.tag == "Goal")
+        {
+            winLoseBG.gameObject.SetActive(true);
+            winLoseText.color = Color.black;
+            winLoseBG.color = Color.green;
+            winLoseText.text = "You win!";
+            StartCoroutine(LoadScene(3));
+        }
+    }
+
+    IEnumerator LoadScene(float seconds)
+    {
+        yield return new WaitForSeconds(seconds);
+        SceneManager.LoadScene(0);
+    }
+
+    void Update()
+    {
+        if (Input.GetKey(KeyCode.Escape))
+        {
+            SceneManager.LoadScene(0);
+        }
+
+        if (health == 0)
+        {
+            winLoseBG.gameObject.SetActive(true);
+            winLoseText.color = Color.white;
+            winLoseBG.color = Color.red;
+            winLoseText.text = "Game Over!";
+            StartCoroutine(LoadScene(3));
+        }
+    }
+
+    void FixedUpdate()
+    {
+        rb.AddForce(0, 0, speed * Time.deltaTime);
+
+        if (Input.GetKey("d"))
+            rb.AddForce(500 * Time.deltaTime, 0, 0);
+        
+        if (Input.GetKey("a"))
+            rb.AddForce(-500 * Time.deltaTime, 0, 0);
+        
+        if (Input.GetKey("w"))
+            rb.AddForce(0, 0, 500 * Time.deltaTime);
+        
+        if (Input.GetKey("s"))
+            rb.AddForce(0, 0, -500 * Time.deltaTime);
+        
+        if (Input.GetKey(KeyCode.RightArrow))
+            rb.AddForce(500 * Time.deltaTime, 0, 0);
+        
+        if (Input.GetKey(KeyCode.LeftArrow))
+            rb.AddForce(-500 * Time.deltaTime, 0, 0);
+        
+        if (Input.GetKey(KeyCode.UpArrow))
+            rb.AddForce(0, 0, 500 * Time.deltaTime);
+        
+        if (Input.GetKey(KeyCode.DownArrow))
+            rb.AddForce(0, 0, -500 * Time.deltaTime);
+    }
+}
---
+++ 0x04-unity_publishing/README.md
@@ -0,0 +1,34 @@
+# Project folder for 0x04-unity_publishing
+# Specializations - AR/VR ― Unity
+# by Carrie Ybay, Software Engineer at Holberton School
+
+## Learning Objectives
+* How to publish a build
+* How to reorder scenes in a build
+* How to build a standalone application for Windows, Mac, and Linux
+* How to edit a project’s Quality Settings
+* How to add an icon to your build
+* What is the profiler and how to use it to check your project’s performance
+* What to consider when developing for different platforms
+
+## Requirements
+* A README.md file, at the root of the folder of the project
+* Use Unity’s default .gitignore in your project folder
+* Push the entire project folder 0x04-unity_publishing to your repo
+* Scenes and project assets such as Scripts must be organized as described in the tasks
+* In your scripts, all your public classes and their members should have XML documentation tags
+* In your scripts, all your private classes and members should be documented but without XML documentation tags
+
+## Tasks
+<b>0. Quality Settings</b>
+* Clone your 0x03-unity-ui repo into a new folder called 0x04-unity_publishing inside your holbertonschool-unity repo.
+
+* In this project, you will be exploring how to publish a game to play on Windows, Mac, or Linux. If there are any changes or improvements you’d like to make to the maze in this repo, feel free! This project will only be checking your build settings and the final standalone game. If you do make changes to your maze, it’s strongly recommended to have a peer play it to make sure it works as intended.
+
+* Edit the project’s Quality Settings. Your project should have three quality levels.
+
+<b>1. Player Settings</b>
+* Edit the Player Settings for PC, Mac, & Linux Standalone. Unless otherwise specified below, leave settings with their default values. Create a new directory in the Assets folder called Images.
+
+<b>2. Build Settings</b>
+* Create three builds of the maze in a directory called Builds in the root of your project folder (not inside the Assets folder).
