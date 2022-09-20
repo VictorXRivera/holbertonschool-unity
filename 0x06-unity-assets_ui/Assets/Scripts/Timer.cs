20933eb4b50712624e71b29ec3f76fc461547105
VictorXRivera <victorxrivera.professional@gmail.com>
2022-09-20 12:35:16 -0400

    Made new levels and downloaded font + UI pack

---
+++ 0x06-unity-assets_ui/Assets/Scripts/Timer.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Diagnostics;
+using System.Threading;
+using System.Collections;
+using System.Collections.Generic;
+using System;
+
+public class Timer : MonoBehaviour
+{
+    public Text TimerText;
+
+    public Stopwatch timer;
+
+    void Start()
+    {
+        timer = new Stopwatch();
+        timer.Start();
+    }
+
+    void Update()
+    {
+        TimeSpan timespan = timer.Elapsed;
+        TimerText.text = string.Format ("{0:00}:{1:00}.{2:00}", timespan.Minutes, timespan.Seconds, timespan.Milliseconds / 10);
+    }
+}
